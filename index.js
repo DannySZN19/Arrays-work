3f5ffb8389da0ed7dcb02ad1acfff1a6738a2a0f
DannySZN19 <danielchidi100@gmail.com>
2025-06-18 23:17:07 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,72 @@
+const jss1 = ["Karina", "Noah", "Olivia", "Ethan", "Miriam" ]
+const jss2 = [ "Mia", "Lucas", "Isabella", "Mason", "Charlotte" ]
+const jss3 = [ "Logan", "Amelia", "William", "Harper", "Benjamin" ]
+const ss1 = ["Evelyn", "Caleb", "Abigail", "Jaxon", "Emily" ]
+const ss2 = ["Elijah", "Lily", "James", "Madison", "Alex" ]
+const ss3 = [ "Danny", "Victoria", "Gabriel", "Jessica", "Roy" ]
+
+//jss1
+console.log(jss1)
+//INDEX
+console.log(jss1[1])
+//PUSH
+jss1.push("Jane")
+console.log(jss1)
+//SHIFT
+jss1.shift("Karina")
+console.log(jss1)
+
+//jss2
+console.log(jss2)
+//INDEX
+console.log(jss2[2])
+//POP
+jss2.pop("Charlotte")
+console.log(jss2)
+//UNSHIFT
+jss2.unshift("Judith")
+console.log(jss2)
+
+//jss3
+console.log(jss3)
+//INDEX
+console.log(jss3[3])
+//POP
+jss3.pop("Benjamin")
+console.log(jss3)
+//UNSHIFT
+jss3.unshift("Brian")
+console.log(jss3)
+
+//ss1
+console.log(ss1)
+//INDEX
+console.log(ss1[4])
+//POP
+ss1.pop("Emily")
+console.log(ss1)
+//UNSHIFT
+ss1.unshift("Kayla")
+console.log(ss1)
+
+//ss2
+console.log(ss2)
+//INDEX
+console.log(ss2[0])
+//POP
+ss2.pop("Alex")
+console.log(ss2)
+//UNSHIFT
+ss2.unshift("Daniya")
+console.log(ss2)
+
+//ss3
+console.log(ss3)
+//INDEX
+console.log(ss3[1])
+//POP
+ss3.pop("Roy")
+console.log(ss3)
+//UNSHIFT
+ss3.unshift("Sandra")
+console.log(ss3)
